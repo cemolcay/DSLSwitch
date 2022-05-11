58a3184a867873b5b78855806858a0b00f59e77a
Cem Olcay <ccemolcay@gmail.com>
2022-05-11 19:59:14 +0300

    Initial Commit

---
+++ DSLSwitch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DSLSwitch
+//
+//  Created by Cem Olcay on 5/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DSLSwitch/DSLSwitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  DSLSwitchApp.swift
+//  DSLSwitch
+//
+//  Created by Cem Olcay on 5/11/22.
+//
+
+import SwiftUI
+
+@main
+struct DSLSwitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
